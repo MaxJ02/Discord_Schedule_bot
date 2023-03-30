9a3fd586658132e6579c907b5e01efb4644dec4c
Max <114068285+MaxJ02@users.noreply.github.com>
2023-03-30 14:40:14 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,34 @@
+import discord
+import os
+from selenium import webdriver
+import time
+import pyautogui
+
+# Defines the intents the bot will use
+intents = discord.Intents.default()
+
+# If you need to enable any additional intents, uncomment the lines below and set them to True
+# intents.members = True
+# intents.guilds = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('Bot is ready to cook')
+    channel = client.get_channel(1234567890) #Replace with the desired discord channel ID. Can only be extracted by the server owner or server administrators.
+    driver = webdriver.Chrome()
+    driver.get('https://web.skola24.se/timetable/timetable-viewer/studiumyrgo.skola24.se/Yrgo%20L%C3%A4rdomsgatan/')
+    time.sleep(5)  # wait for 5 seconds
+    pyautogui.moveTo(1000, 440, duration = 1)
+    pyautogui.click(1000, 440)
+    pyautogui.typewrite("ELA22")            #Replace with desired class ID. 
+    pyautogui.typewrite(["enter"])
+    time.sleep(5)
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)") #scrolls down all the way
+    time.sleep(2) # waits another 2 seconds to let the page settle
+    driver.save_screenshot('screenshot.png')
+    await channel.send(file=discord.File('screenshot.png'))
+    driver.quit()
+
+client.run('abcdefghijklmnopqrstuvwxyz')    #Replace wtih your discord bots private token. 
