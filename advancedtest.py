9742d4f78e0e6d4349084e5aee1ff1ef9bc37e00
Max <114068285+MaxJ02@users.noreply.github.com>
2023-03-30 17:22:10 +0200

    Update advancedtest.py

--- advancedtest.py
+++ advancedtest.py
@@ -5,11 +5,6 @@ from selenium import webdriver
 import time
 import pyautogui
 
-# Defines the intents the bot will use
-
-#intents = discord.Intents.default()
-#client = discord.Client(intents=intents)
-
 bot = commands.Bot(command_prefix = "!", intents = discord.Intents.all())
 
 BOT_TOKEN = ""
@@ -20,26 +15,25 @@ async def on_ready():
     print("Bot is alive")
     channel = bot.get_channel(CHANNEL_ID)
 
-# Lists all active commands
 @bot.command()
 async def commands(ctx):
     await ctx.send("```schema```")
 
 @bot.command()
 async def schema(ctx):
-    channel = client.get_channel(1234567890) #Replace with the desired discord channel ID. Can only be extracted by the server owner or server administrators.
+    channel = client.get_channel(1234567890)
     driver = webdriver.Chrome()
-    driver.get('https://web.skola24.se/timetable/timetable-viewer/studiumyrgo.skola24.se/Yrgo%20L%C3%A4rdomsgatan/') #Can be replaced with the URL to your skola24 domain.
+    driver.get('https://web.skola24.se/timetable/timetable-viewer/studiumyrgo.skola24.se/Yrgo%20L%C3%A4rdomsgatan/') 
     time.sleep(5)  # wait for 5 seconds
-    pyautogui.moveTo(1000, 440, duration = 1) #Actual x and y values might need to be tinkered with depending on your screens resolution.
-    pyautogui.click(1000, 440)                # ^ Can be achieved through simple trial and error 
-    pyautogui.typewrite("ELA22")            #Replace with desired class ID. 
+    pyautogui.moveTo(1000, 440, duration = 1)
+    pyautogui.click(1000, 440)                
+    pyautogui.typewrite("ELA22")             
     pyautogui.typewrite(["enter"])
     time.sleep(5)
-    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)") #scrolls down all the way
-    time.sleep(2) # waits another 2 seconds to let the page settle
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)") 
+    time.sleep(2) 
     driver.save_screenshot('screenshot.png')
     await channel.send(file=discord.File('screenshot.png'))
     driver.quit()
 
-bot.run(BOT_TOKEN)    #Replace wtih your discord bots private token. 
+bot.run(BOT_TOKEN)
