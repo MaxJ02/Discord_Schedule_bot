e7a56671c68c6d762e6fe4e7a7e7dd141465f870
Max <114068285+MaxJ02@users.noreply.github.com>
2023-04-03 17:59:11 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -5,50 +5,55 @@ from selenium.webdriver.chrome.options import Options
 import time
 import pyautogui
 import datetime
-from datetime import date 
+from datetime import date
 
 bot = commands.Bot(command_prefix = "!", intents = discord.Intents.all())
 
-BOT_TOKEN = "BOT_TOKEN_HERE" #Replace with your discord bots token. This token is private and should not be uploaded to github.
-CHANNEL_ID = 1234567890      #Channel ID for the desired discord channel. Enable developer settings in discord to extract this.
+BOT_TOKEN = "abc123def456"  #Replace with your discord bots token. This token is private and should not be uploaded to github.
 
-date = date.today()
+# Checks the current day of the week. If its a weekday (mon - fri), the current week & schedule will be printed. Else, The next weeks number and schedule will be printed.
+def get_week_number(date):
+    
+    if date.weekday() < 5:
+        return date.isocalendar().week
+    else:
+        return date.isocalendar().week + 1
 
-week_number = date.isocalendar().week +1
+today = datetime.date.today()
+week_number = get_week_number(today)
 
 @bot.event
 async def on_ready():
-    print("Bot is alive and ready to serve")
+    print("Bot is alive")
 
 @bot.command()
 async def commands(ctx):
-    await ctx.send("``` schema, commands, jagre ```")
+    await ctx.send("``` schema, commands, pihl, jagre ```")
 
 @bot.command()
 async def jagre(ctx):
     await ctx.send("Använd ditt bonnaförnuft nu!")
 
 @bot.command()
-async def test(ctx):
-    await ctx.send("test")
+async def pihl(ctx):
+    await ctx.send("")
 
 @bot.command()
-async def schema(ctx):
-    channel = bot.get_channel(CHANNEL_ID)
+async def schema(ctx, classid="ELA22"): # Asks for a classid, if none is provided, the default value of ELA22 will be used. 
     driver = webdriver.Chrome()
     driver.get('https://web.skola24.se/timetable/timetable-viewer/studiumyrgo.skola24.se/Yrgo%20L%C3%A4rdomsgatan/') 
     driver.set_window_size(1024, 768)
     time.sleep(3)
     pyautogui.moveTo(1000, 440, duration = 1)
     pyautogui.click(1000, 440)                
-    pyautogui.typewrite("ELA22")               # Update with actual class ID (Ie ELA22, EI21 etc)  
+    pyautogui.typewrite(classid)             
     pyautogui.typewrite(["enter"])
     time.sleep(3)
     driver.execute_script("window.scrollTo(0,document.body.scrollHeight)") 
     time.sleep(2)
-    await ctx.send(f"Schema vecka {week_number}")
+    await ctx.send(f"Schema vecka {week_number} för {classid}")
     driver.save_screenshot('screenshot.png')
-    await channel.send(file=discord.File('screenshot.png'))
+    await ctx.send(file=discord.File('screenshot.png'))
     driver.quit()
 
 bot.run(BOT_TOKEN)
