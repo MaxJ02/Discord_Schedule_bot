20d5be8176c239e3d1f7ee65cc4cc9f1f88f561d
Max <114068285+MaxJ02@users.noreply.github.com>
2023-04-03 20:41:59 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -5,50 +5,66 @@ from selenium.webdriver.chrome.options import Options
 import time
 import pyautogui
 import datetime
-from datetime import date 
+from datetime import date
 
 bot = commands.Bot(command_prefix = "!", intents = discord.Intents.all())
 
-BOT_TOKEN = "BOT_TOKEN_HERE" #Replace with your discord bots token. This token is private and should not be uploaded to github.
-CHANNEL_ID = 1234567890      #Channel ID for the desired discord channel. Enable developer settings in discord to extract this.
+BOT_TOKEN = ""
 
-date = date.today()
+def get_week_number(date):
+    
+    if date.weekday() < 5:
+        return date.isocalendar().week
+    else:
+        return date.isocalendar().week + 1
 
-week_number = date.isocalendar().week +1
+today = datetime.date.today()
+week_number = get_week_number(today)
 
 @bot.event
 async def on_ready():
-    print("Bot is alive and ready to serve")
+    print("Bot is alive")
 
 @bot.command()
-async def commands(ctx):
-    await ctx.send("``` schema, commands, jagre ```")
-
-@bot.command()
-async def jagre(ctx):
-    await ctx.send("Använd ditt bonnaförnuft nu!")
-
-@bot.command()
-async def test(ctx):
-    await ctx.send("test")
-
-@bot.command()
-async def schema(ctx):
-    channel = bot.get_channel(CHANNEL_ID)
+async def schema(ctx, classid="ELA22"):
     driver = webdriver.Chrome()
     driver.get('https://web.skola24.se/timetable/timetable-viewer/studiumyrgo.skola24.se/Yrgo%20L%C3%A4rdomsgatan/') 
     driver.set_window_size(1024, 768)
     time.sleep(3)
     pyautogui.moveTo(1000, 440, duration = 1)
     pyautogui.click(1000, 440)                
-    pyautogui.typewrite("ELA22")               # Update with actual class ID (Ie ELA22, EI21 etc)  
+    pyautogui.typewrite(classid)             
     pyautogui.typewrite(["enter"])
     time.sleep(3)
     driver.execute_script("window.scrollTo(0,document.body.scrollHeight)") 
     time.sleep(2)
-    await ctx.send(f"Schema vecka {week_number}")
+    await ctx.send(f"Schema vecka {week_number} för {classid}")
     driver.save_screenshot('screenshot.png')
-    await channel.send(file=discord.File('screenshot.png'))
-    driver.quit()
+    await ctx.send(file=discord.File('screenshot.png'))
+    driver.quit()    
+
+@bot.command()
+async def commands(ctx):
+    await ctx.send("``` schema, commands, pihl, jagre, hampus, polski, polskaharrypotter ```")
+
+@bot.command()
+async def jagre(ctx):
+    await ctx.send("Använd ditt bonnaförnuft nu!")
+
+@bot.command()
+async def pihl(ctx):
+    await ctx.send("so anyway, here's wonderwall: https://youtu.be/FVdjZYfDuLE")
+
+@bot.command()
+async def hampus(ctx):
+    await ctx.send("git gud")
+
+@bot.command()
+async def polski(ctx):
+    await ctx.send("https://img.ifunny.co/videos/4e8e61a2e0287424f8ddedf42d3d3720227e98bc7e6d4cae3fc2588a4a803ab2_1.mp4")    
+
+@bot.command()
+async def polskaharrypotter(ctx):
+    await ctx.send("https://youtu.be/1puKg2thrtA")
 
 bot.run(BOT_TOKEN)
