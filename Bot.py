27d68d109f0bef73a2d6707742ee0ebc72143f83
Max <114068285+MaxJ02@users.noreply.github.com>
2023-04-06 20:08:36 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
@@ -26,50 +27,67 @@ week_number = get_week_number(today)
 @bot.event
 async def on_ready():
     print("Bot is alive")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
 
 @bot.command()
 async def schema(ctx, classid="ELA22"): # Asks for a classid to be sent after !schema, if none is provided, the default value of ELA22 will be used.
     driver = webdriver.Chrome()
     driver.get('https://web.skola24.se/timetable/timetable-viewer/studiumyrgo.skola24.se/Yrgo%20L%C3%A4rdomsgatan/') #replace with desired skola24 url.
     driver.set_window_size(1024, 768)
-    time.sleep(3)
-    pyautogui.moveTo(1000, 440, duration = 1)
-    pyautogui.click(1000, 440)                
+    time.sleep(1)
+    pyautogui.moveTo(850, 420, duration = 1) #Might need to be adjusted with trial and error.
+    pyautogui.click(850, 420)                #ditto
     pyautogui.typewrite(classid)             
     pyautogui.typewrite(["enter"])
-    time.sleep(3)
+    time.sleep(1)
     driver.execute_script("window.scrollTo(0,document.body.scrollHeight)") 
-    time.sleep(2)
+    time.sleep(1)
     await ctx.send(f"Schema vecka {week_number} för {classid}")
     driver.save_screenshot('screenshot.png')
     await ctx.send(file=discord.File('screenshot.png'))
-    
-bot.run(BOT_TOKEN) 
-#The ones below are just fun extras.
-    
-# Fun inside jokes    
+
+bot.run(BOT_TOKEN)    
+    #The ones below are just fun extras.
+
+#@bot.tree.command(name="commands")
+#async def commands(interaction: discord.Interaction):
+#    await interaction.response.send_message(f"schema, commands, pihl, jagre, hampus, polski, polskaharrypotter") 
+#
+#        
 #@bot.command()
-#async def commands(ctx):
+#async def commands2(ctx):
 #    await ctx.send("``` schema, commands, pihl, jagre, hampus, polski, polskaharrypotter ```")
 #
 #@bot.command()
 #async def jagre(ctx):
 #    await ctx.send("Använd ditt bonnaförnuft nu!")
 # 
-# @bot.command()
-# async def pihl(ctx):
+#@bot.command()
+#async def pihl(ctx):
 #     await ctx.send("so anyway, here's wonderwall: https://youtu.be/FVdjZYfDuLE")
 # 
-# @bot.command()
-# async def hampus(ctx):
-#     await ctx.send("git gud")
+#@bot.command()
+#async def hampus(ctx):
+#   await ctx.send("git gud")
+#
+#@bot.command()
+#async def polski(ctx):
+#    await ctx.send("https://img.ifunny.co/videos/4e8e61a2e0287424f8ddedf42d3d3720227e98bc7e6d4cae3fc2588a4a803ab2_1.mp4")    
 # 
-# @bot.command()
-# async def polski(ctx):
-#     await ctx.send("https://img.ifunny.co/videos/4e8e61a2e0287424f8ddedf42d3d3720227e98bc7e6d4cae3fc2588a4a803ab2_1.mp4")    
-# 
-# @bot.command()
-# async def polskaharrypotter(ctx):
+#@bot.command()
+#async def polskaharrypotter(ctx):
 #     await ctx.send("https://youtu.be/1puKg2thrtA")
-# 
-# bot.run(BOT_TOKEN)
+#    
+#@bot.tree.command(name="hello")
+#async def hello(interaction: discord.Interaction):
+#    await interaction.response.send_message(f"Tjena {interaction.user.mention}! Detta är ett slash commando!")
+#    
+#@bot.tree.command(name="ping")
+#async def ping(interaction: discord.Interaction):
+#    await interaction.response.send_message(f"pong")         
+#       
+#bot.run(BOT_TOKEN)
